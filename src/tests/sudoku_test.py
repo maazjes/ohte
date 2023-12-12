9b5f208c57376dd795edd87183ef96ce2aa3b37c
Marius Marius <marzusness@gmail.com>
2023-12-13 00:07:55 +0200

    Lisää testejä

--- src/tests/sudoku_test.py
+++ src/tests/sudoku_test.py
@@ -3,6 +3,18 @@ from Sudoku import Sudoku
 
 
 class TestHelpers(unittest.TestCase):
+    def setUp(self):
+        self.sudoku = Sudoku()
+
     def test_generate_sudoku_creates_valid_sudoku(self):
-        sudoku = Sudoku()
-        self.assertTrue(sudoku.validate())
+        self.assertTrue(self.sudoku.validate())
+
+    def test_generate_sudoku(self):
+        new_sudoku = self.sudoku.generate_sudoku(4)
+        self.assertEqual(len(new_sudoku), 16)
+        self.assertEqual(len(new_sudoku[0]), 16)
+
+    def test_set_empty_cells(self):
+        self.sudoku.set_empty_cells(10)
+        empty_cells = sum(row.count(0) for row in self.sudoku.board)
+        self.assertEqual(empty_cells, 10)
