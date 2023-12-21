c896c25b6b30d2f2f4b2d68bbf082e02ae74f3e6
Marius Marius <marzusness@gmail.com>
2023-12-21 15:13:40 +0200

    Exclude tests and tasks from mypy

--- src/tests/database_test.py
+++ src/tests/database_test.py
@@ -3,10 +3,10 @@ from database import Database
 
 
 class TestDatabase(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.database = Database("testing.db")
 
-    def test_insert_game(self) -> None:
+    def test_insert_game(self):
         moves = 10
         empty_cells = 5
 
@@ -18,7 +18,7 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(last_row[2], moves, "Moves value mismatch")
         self.assertEqual(last_row[3], empty_cells, "Empty cells value mismatch")
 
-    def test_get_games(self) -> None:
+    def test_get_games(self):
         self.database.insert_game(20, 20, 10)
         records = self.database.get_games()
         self.assertTrue(len(records) > 0, "No records retrieved")
--- src/tests/sudoku_test.py
+++ src/tests/sudoku_test.py
@@ -16,27 +16,27 @@ valid_board = [
 
 
 class TestSudoku(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.sudoku = Sudoku()
 
-    def test_cell_is_valid(self) -> None:
+    def test_cell_is_valid(self):
         self.sudoku.board = copy.deepcopy(valid_board)
         self.sudoku.board[0][0] = 0
         self.assertTrue(self.sudoku.cell_is_valid(0, 0, 5))
         self.assertFalse(self.sudoku.cell_is_valid(0, 0, 2))
 
-    def test_validate_with_valid_board(self) -> None:
+    def test_validate_with_valid_board(self):
         self.sudoku.set_empty_cells(0)
         self.assertTrue(self.sudoku.validate(), "Initial board should be valid")
         self.sudoku.board = valid_board
         self.assertTrue(self.sudoku.validate(), "Valid board should be valid")
 
-    def test_validate_with_invalid_board(self) -> None:
+    def test_validate_with_invalid_board(self):
         self.sudoku.set_empty_cells(0)
         self.sudoku.board[0][0] = self.sudoku.board[1][1]
         self.assertFalse(self.sudoku.validate(), "Modified board should be invalid")
 
-    def test_insert_number(self) -> None:
+    def test_insert_number(self):
         valid = self.sudoku.insert_number(0, 0, self.sudoku.board[0][1])
         self.assertEqual(
             self.sudoku.board[0][0],
@@ -45,7 +45,7 @@ class TestSudoku(unittest.TestCase):
         )
         self.assertFalse(valid, "Inserted number should be invalid")
 
-    def test_set_base(self) -> None:
+    def test_set_base(self):
         self.sudoku.set_base(4)
         self.assertEqual(
             len(self.sudoku.board), 16, "Board size should change with base"
@@ -54,17 +54,17 @@ class TestSudoku(unittest.TestCase):
             len(self.sudoku.board[0]), 16, "Board size should change with base"
         )
 
-    def test_generate_sudoku(self) -> None:
+    def test_generate_sudoku(self):
         new_sudoku = self.sudoku.generate_sudoku(4)
         self.assertEqual(len(new_sudoku), 16, "Amount of rows should be 16")
         self.assertEqual(len(new_sudoku[0]), 16, "Amount of columns should be 16")
 
-    def test_set_empty_cells(self) -> None:
+    def test_set_empty_cells(self):
         self.sudoku.set_empty_cells(10)
         empty_cells = sum(row.count(0) for row in self.sudoku.board)
         self.assertEqual(empty_cells, 10, "There should be 10 empty cells")
 
-    def test_str_method(self) -> None:
+    def test_str_method(self):
         self.sudoku.board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
         expected_output = "[1, 2, 3]\n[4, 5, 6]\n[7, 8, 9]"
